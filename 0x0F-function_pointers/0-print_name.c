5c882d47587e625bce01bb0ff8470784e36e7447
rhydian001 <rhydian001@gmail.com>
2022-11-18 03:59:37 -0800

    2-int_index.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
 
 /**
- *  * print_name - a function that prints a name.
- *   * @name: the name given
- *    * @f: function of name
- *     *
- *      * Rteurn: void
- *       */
+ * print_name - a function that prints a name.
+ * @name: the name given
+ * @f: function of name
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
-		if (name != NULL && f != NULL)
+	if (name != NULL && f != NULL)
+		if(name);
+}	
 
-					f(name);
-}
